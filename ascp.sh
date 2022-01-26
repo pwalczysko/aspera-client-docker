7ce25a530b6c6a0a02d07a57c58c0db39f7ea54b
Gregory Way <gregory.way@gmail.com>
2022-01-26 16:07:06 -0700

    add ASCP_OPTION_LIMIT arg to ascp.sh

--- ascp.sh
+++ ascp.sh
@@ -6,6 +6,13 @@ set -u
 STUDY=$1; shift
 SOURCE=$1; shift
 TARGET=$1; shift
+ASCP_OPTION_LIMIT=$1; shift
+
+# If ASCP_OPTION_LIMIT is provided, then reset ASCP environment variable limit
+# that was set in the Dockerfile
+if [[ -n $ASCP_OPTION_LIMIT ]]; then
+  ASCP_LIMIT=$ASCP_OPTION_LIMIT
+fi
 
 echo /home/data/.aspera/connect/bin/ascp \
     -P$ASCP_PORT \
