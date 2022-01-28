ae9200f789c012e26b960d19d0fbaa3ff88bd24c
Gregory Way <gregory.way@gmail.com>
2022-01-28 11:52:14 -0700

    remove ASCP limit (in favor of setting docker env) and add k option

--- ascp.sh
+++ ascp.sh
@@ -6,18 +6,12 @@ set -u
 STUDY=$1; shift
 SOURCE=$1; shift
 TARGET=$1; shift
-ASCP_OPTION_LIMIT=$1; shift
-
-# If ASCP_OPTION_LIMIT is provided, then reset ASCP environment variable limit
-# that was set in the Dockerfile
-if [[ -n $ASCP_OPTION_LIMIT ]]; then
-  ASCP_LIMIT=$ASCP_OPTION_LIMIT
-fi
 
 echo /home/data/.aspera/connect/bin/ascp \
     -P$ASCP_PORT \
     -l "$ASCP_LIMIT" \
     -i "$ASCP_KEY" \
+    -k "$ASCP_RETRANSFER_FILE" \
     "$@" \
     "$STUDY@$ASCP_SERVER:$SOURCE" "$TARGET"
 
@@ -25,5 +19,6 @@ echo /home/data/.aspera/connect/bin/ascp \
     -P$ASCP_PORT \
     -l "$ASCP_LIMIT" \
     -i "$ASCP_KEY" \
+    -k "$ASCP_RETRANSFER_FILE" \
     "$@" \
     "$STUDY@$ASCP_SERVER:$SOURCE" "$TARGET"
